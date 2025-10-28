688bce08096099d1d50273c4e6682feaa9448444
King-3 <75344974+coderking3@users.noreply.github.com>
2025-10-28 06:08:47 +0000

    :rocket: feat: add exports for new utility functions.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,22 @@ export { bubblingSort, group, iterate, sort } from './array'
 export { sleep, tryit } from './async'
 export { compose, currying, debounce, throttle } from './curry'
 export { formatDate } from './date'
-export { padNumber } from './number'
-export { deepClone, shallowClone } from './object'
-export { random, setSerialInterval, toQueryString, uid } from './other'
+export { padNumber, toFloat, toInt } from './number'
+export {
+  createTransformer,
+  deepClone,
+  shallowClone,
+  transformObjectValues
+} from './object'
+export {
+  getQueryParams,
+  random,
+  setQueryParams,
+  setSerialInterval,
+  toQueryString,
+  tryParse,
+  uid
+} from './other'
 export { insertAt, transformCase } from './string'
 export {
   getType,
